ec679e56b6cb33a781ba9d2aee6d4a015ba1d956
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-20 17:27:05 +0200

    Aggiunta prima parte info footer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 var root = new Vue({
-    el: '#root',
+    el: '#header-vue',
      data: {
      //Struttura dati per link nav
      linksNav: [
@@ -16,9 +16,38 @@ var root = new Vue({
         },
      
     
-       methods: {
-    
-    
-       }
-    
     });
+
+
+    var root = new Vue({
+      el: '#footer-vue',
+       data: {
+      //Uso un oggetto come struttura dati per l'indirizzo/e-mail/telefono
+       findUs: {
+         Address: '12345 North Main Street',
+         Telephone: '1.800.555.6789',
+         Email: 'info@your-domain.com',
+       },
+       //Uso un array come struttura per le domande
+       questions: [
+        'Train with free weights or your body weight?',
+        'Notitional advice that will keep you training'
+       ],
+
+       openingHours: [
+          {
+            range: 'Monday-Friday',
+            schedule: '9:00 - 21:00'
+          },
+          {
+            range: 'Saturday-Sunday',
+            schedule: '9:00 - 19:00'
+          },
+       ]
+     
+      
+      
+          },
+       
+      
+      });
